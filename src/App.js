d9e0aee64e23111514ca60e1ecdfe6feab05c064
Jose Zuñiga <zuniga.joseignacio@gmail.com>
2020-08-24 02:43:26 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from 'config/theme'
+import { routes } from 'routes'
+import { FeedbackProvider } from 'context'
+import Layout from 'screens/Layout'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<FeedbackProvider>
+				<Layout>
+					<Router>
+						<Switch>
+							{routes.map(({ path, component }) => (
+								<Route key={path} path={path} exact component={component} />
+							))}
+							<Redirect to={routes[0].path} />
+						</Switch>
+					</Router>
+				</Layout>
+			</FeedbackProvider>
+		</ThemeProvider>
+	)
 }
 
-export default App;
+export default App
